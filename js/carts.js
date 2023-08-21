8716377e66688adf91a94c4e2badb462c469b689
Tonmoy <shahariartonmoy17@gmail.com>
2023-08-21 06:19:33 +0600

    All calculation done and showing them in right place done

--- js/carts.js
+++ js/carts.js
@@ -1,9 +1,19 @@
-addItemsInDiv('card-1', 'cart-items', 'card-title-1')
-addItemsInDiv('card-2', 'cart-items', 'card-title-2')
-addItemsInDiv('card-3', 'cart-items', 'card-title-3')
-addItemsInDiv('card-4', 'cart-items', 'card-title-4')
-addItemsInDiv('card-5', 'cart-items', 'card-title-5')
-addItemsInDiv('card-6', 'cart-items', 'card-title-6')
-addItemsInDiv('card-7', 'cart-items', 'card-title-7')
-addItemsInDiv('card-8', 'cart-items', 'card-title-8')
-addItemsInDiv('card-9', 'cart-items', 'card-title-9')
+addItemsInDiv('card-1', 'cart-items', 'card-title-1');
+addItemsInDiv('card-2', 'cart-items', 'card-title-2');
+addItemsInDiv('card-3', 'cart-items', 'card-title-3');
+addItemsInDiv('card-4', 'cart-items', 'card-title-4');
+addItemsInDiv('card-5', 'cart-items', 'card-title-5');
+addItemsInDiv('card-6', 'cart-items', 'card-title-6');
+addItemsInDiv('card-7', 'cart-items', 'card-title-7');
+addItemsInDiv('card-8', 'cart-items', 'card-title-8');
+addItemsInDiv('card-9', 'cart-items', 'card-title-9');
+
+getPriceFromCard('card-1', 'card-1-price');
+getPriceFromCard('card-2', 'card-2-price');
+getPriceFromCard('card-3', 'card-3-price');
+getPriceFromCard('card-4', 'card-4-price');
+getPriceFromCard('card-5', 'card-5-price');
+getPriceFromCard('card-6', 'card-6-price');
+getPriceFromCard('card-7', 'card-7-price');
+getPriceFromCard('card-8', 'card-8-price');
+getPriceFromCard('card-9', 'card-9-price');
--- js/utilities.js
+++ js/utilities.js
@@ -1,21 +1,83 @@
-let childCount =  0;
-function addItemsInDiv(clickedElementId, divIdWhereAdd, idWhatAdd ){
-    const whereAdd = document.getElementById(divIdWhereAdd);
-    
-    const whatAdd = document.getElementById(idWhatAdd).innerText;
+//function for adding new elements in a div
+let childCount = 0;
+let totalPrice = getValueFromElement("total-price");
+function addItemsInDiv(clickedElementId, divIdWhereAdd, idWhatAdd) {
+  const whereAdd = document.getElementById(divIdWhereAdd);
 
-    document.getElementById(clickedElementId).addEventListener('click', function(){
-        const newElement = document.createElement('p');
-        newElement.innerHTML = `
-        ${childCount+1} . ${whatAdd}
+  const whatAdd = document.getElementById(idWhatAdd).innerText;
+
+  document
+    .getElementById(clickedElementId)
+    .addEventListener("click", function () {
+      const newElement = document.createElement("p");
+      newElement.innerHTML = `
+        ${childCount + 1} . ${whatAdd}
     `;
-    newElement.classList.add('text-2xl')
-    newElement.classList.add('font-medium')
-    newElement.classList.add('mb-4')
-    const hiddenHr = document.getElementById('hidden-hr');
-    hiddenHr.classList.remove('hidden');
+      newElement.classList.add("text-2xl");
+      newElement.classList.add("font-medium");
+      newElement.classList.add("mb-4");
+      const hiddenHr = document.getElementById("hidden-hr");
+      hiddenHr.classList.remove("hidden");
 
-    whereAdd.appendChild(newElement);
-    childCount++;
-    });  
+      whereAdd.appendChild(newElement);
+      childCount++;
+    });
+}
+
+//function for getting value form an element
+function getValueFromElement(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueString = element.innerText;
+  const elementValue = parseFloat(elementValueString);
+
+  return elementValue;
+}
+
+//function for setting vale to an element
+function setValueToElement(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
+//function for getting price after clicking on card
+function getPriceFromCard(cardId, cardPriceId) {
+  document.getElementById(cardId).addEventListener("click", function () {
+    const cardPrice = getValueFromElement(cardPriceId);
+    totalPrice = totalPrice + cardPrice;
+    //checking purchasable or not
+    const makePurchaseButton = document.getElementById("make-purchase");
+    if (totalPrice > 0.0) {
+      makePurchaseButton.removeAttribute("disabled");
+      makePurchaseButton.classList.remove("text-gray-300");
+      makePurchaseButton.classList.add("text-white");
+    }
+    //checking promocode
+    const promoCodeBtn = document.getElementById('promocode-btn');
+    if(totalPrice >= 200.00){
+        promoCodeBtn.removeAttribute('disabled');
+        promoCodeBtn.classList.remove('text-gray-300');           
+        promoCodeBtn.classList.add('text-white');
+    }
+    else{
+        promoCodeBtn.setAttribute('disabled', true);
+        promoCodeBtn.classList.remove('text-white');
+        promoCodeBtn.classList.add('text-gray-300');
+    }
+    //after typing the promocode 20% discount calculation
+    document.getElementById('promocode-field').addEventListener('keyup', function(event){
+        const promoCodeFiledText = event.target.value;
+        if(promoCodeFiledText === 'SELL200'){
+            document.getElementById('promocode-btn').addEventListener('click', function(){
+                const discount = totalPrice * 0.2;
+                totalPriceDiscount = totalPrice - discount;
+                setValueToElement('discount', discount.toFixed(2));
+                setValueToElement('total', totalPriceDiscount.toFixed(2))
+            });
+        }
+        else{
+               
+        }
+    })
+    setValueToElement("total-price", totalPrice.toFixed(2));
+  });
 }
